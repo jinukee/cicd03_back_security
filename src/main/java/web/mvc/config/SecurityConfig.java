a835746d45b972560f0bb76c38ec9747e5186a79
jinukee <jinukee333@gmail.com>
2025-12-10 11:07:21 +0900

    cors allow my domain

--- src/main/java/web/mvc/config/SecurityConfig.java
+++ src/main/java/web/mvc/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package web.mvc.config;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+
 import web.mvc.jwt.JWTFilter;
 import web.mvc.jwt.JWTUtil;
 import web.mvc.jwt.LoginFilter;
@@ -28,74 +30,75 @@ import java.util.Collections;
 @Slf4j
 public class SecurityConfig {
 
-    //AuthenticationManager 가 인자로 받을 AuthenticationConfiguraion 객체 생성자
-    private final AuthenticationConfiguration authenticationConfiguration;
-    private final JWTUtil jwtUtil;
-    //AuthenticationManager Bean 등록
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
+	//AuthenticationManager 가 인자로 받을 AuthenticationConfiguraion 객체 생성자
+	private final AuthenticationConfiguration authenticationConfiguration;
+	private final JWTUtil jwtUtil;
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        log.info("bCryptPasswordEncoder call.....");
-        return new BCryptPasswordEncoder();
-    }
+	//AuthenticationManager Bean 등록
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
+	}
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        log.info("SecurityFilterChain filterChain(HttpSecurity http) call.....");
-       /////////////////////////////////
-        //CORS 설정
-        http.cors((corsCustomizer ->
-                corsCustomizer.configurationSource(new CorsConfigurationSource()
-                {
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                        CorsConfiguration configuration = new CorsConfiguration();
-                        //configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
-                        //configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:4173"));
-                        configuration.setAllowedOrigins(Arrays.asList("http://13.125.128.10", "http://13.125.128.10:80"));
-                        //configuration.setAllowedOrigins(Arrays.asList("http://jinook.kro.kr", "https://jinook.kro.kr"));
-                        configuration.setAllowedMethods(Collections.singletonList("*"));
-                        configuration.setAllowCredentials(true);
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		log.info("bCryptPasswordEncoder call.....");
+		return new BCryptPasswordEncoder();
+	}
 
-                        configuration.setAllowedHeaders(Collections.singletonList("*"));
-                        configuration.setMaxAge(3600L);
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		log.info("SecurityFilterChain filterChain(HttpSecurity http) call.....");
+		/////////////////////////////////
+		//CORS 설정
+		http.cors((corsCustomizer ->
+			corsCustomizer.configurationSource(new CorsConfigurationSource() {
+				@Override
+				public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+					CorsConfiguration configuration = new CorsConfiguration();
+					//configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+					//configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:4173"));
+					// configuration.setAllowedOrigins(Arrays.asList("http://13.125.128.10", "http://13.125.128.10:80"));
+					configuration.setAllowedOrigins(Arrays.asList("http://jinook.kro.kr", "https://jinook.kro.kr"));
+					configuration.setAllowedMethods(Collections.singletonList("*"));
+					configuration.setAllowCredentials(true);
 
-                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
-                        return configuration;
-                    }
-                })));
-        ////////////////////////////////////
-        //csrf disable
-        http.csrf((auth) -> auth.disable()); //csrf공격을 방어하기 위한 토큰 주고 받는 부분을 비활성화!
+					configuration.setAllowedHeaders(Collections.singletonList("*"));
+					configuration.setMaxAge(3600L);
 
-        //Form 로그인 방식 disable -> React, JWT 인증 방식으로 변겨예정
-        //disable 를 설정하면 시큐리티의 UsernamePasswordAuthenticationFilter비활성됨.
-        http.formLogin((auth) -> auth.disable());
+					configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+					return configuration;
+				}
+			})));
+		////////////////////////////////////
+		//csrf disable
+		http.csrf((auth) -> auth.disable()); //csrf공격을 방어하기 위한 토큰 주고 받는 부분을 비활성화!
 
-        //http basic 인증 방식 disable
-        http.httpBasic((auth) -> auth.disable());
+		//Form 로그인 방식 disable -> React, JWT 인증 방식으로 변겨예정
+		//disable 를 설정하면 시큐리티의 UsernamePasswordAuthenticationFilter비활성됨.
+		http.formLogin((auth) -> auth.disable());
 
-        //경로별 인가 작업
-        http.authorizeHttpRequests((auth) ->
-                auth
-                        .requestMatchers("/test", "/members", "/members/**", "/boards").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                        .anyRequest().authenticated());
+		//http basic 인증 방식 disable
+		http.httpBasic((auth) -> auth.disable());
 
-        //세션 설정 - JWT 를 이용 할것이기 때문에 stateless 설정
-        http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		//경로별 인가 작업
+		http.authorizeHttpRequests((auth) ->
+			auth
+				.requestMatchers("/test", "/members", "/members/**", "/boards").permitAll()
+				.requestMatchers("/admin").hasRole("ADMIN")
+				.anyRequest().authenticated());
 
-        //필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager()
-        //메소드에 authenticationConfiguration 객체를 넣어야 함)
-       //addFilterAt 은 UsernamePasswordAuthenticationFilter 의 자리에 LoginFilter 가 실행되도록 설정하는 것
-        //JWTFilter 등록
-        http.addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
-        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
+		//세션 설정 - JWT 를 이용 할것이기 때문에 stateless 설정
+		http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        return http.build();
-    }
+		//필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager()
+		//메소드에 authenticationConfiguration 객체를 넣어야 함)
+		//addFilterAt 은 UsernamePasswordAuthenticationFilter 의 자리에 LoginFilter 가 실행되도록 설정하는 것
+		//JWTFilter 등록
+		http.addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
+		http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil),
+			UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
 }
--- src/main/java/web/mvc/config/WebMvcConfig.java
+++ src/main/java/web/mvc/config/WebMvcConfig.java
@@ -1,26 +1,27 @@
 package web.mvc.config;
+
 import org.springframework.context.annotation.Configuration;
-import 
-org.springframework.web.servlet.config.annotation.CorsRegistry;
-import 
-org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import 
-org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import
+	org.springframework.web.servlet.config.annotation.CorsRegistry;
+import
+	org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import
+	org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
-* WebMvcConfigurer 를 이용해서 @CrossOrigin 글로벌 설정
-* */
+ * WebMvcConfigurer 를 이용해서 @CrossOrigin 글로벌 설정
+ * */
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-     registry.addMapping("/**")
-    // .allowedOrigins("http://localhost:5173","http://localhost:4173")
-     //.allowedOrigins("http://43.200.184.113:5173", "http://43.200.184.113:4173")
-      .allowedOrigins("http://13.125.128.10", "https://13.125.128.10:80")
-      //.allowedOrigins("http://jinook.kro.kr", "https://jinook.kro.kr")
-     .allowedMethods("OPTIONS","GET","POST","PUT","DELETE");
-  }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			// .allowedOrigins("http://localhost:5173","http://localhost:4173")
+			//.allowedOrigins("http://43.200.184.113:5173", "http://43.200.184.113:4173")
+			//  .allowedOrigins("http://13.125.128.10", "https://13.125.128.10:80")
+			.allowedOrigins("http://jinook.kro.kr", "https://jinook.kro.kr")
+			.allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+	}
 }
